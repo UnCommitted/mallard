2e8ad6cd1a1156a3f54d19efa8b7195434f0b1fc
Simon Sellar <simon@ourbusyfamily.net>
2020-04-26 17:17:41 +0930

    Created mallard library and the beginning of the world module.

--- src/bin/railsim.rs
+++ src/bin/railsim.rs
@@ -1,3 +1,8 @@
+use mallard::world::World;
+
+/// railsim binary for testing instantiation of the various components
+/// To be used as a template for later users.
 fn main() {
-    println!("Hello, world!");
+    let temp_world = World;
+    temp_world.temp("blah".to_owned());
 }
