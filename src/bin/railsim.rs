8b8c50a2ce06c87d8a3473e16bc26c6e798ff1df
Simon Sellar <simon@ourbusyfamily.net>
2020-04-26 22:46:06 +0930

    Async main world function starting point
    - Accepts and sends message.

--- src/bin/railsim.rs
+++ src/bin/railsim.rs
@@ -1,8 +1,55 @@
-use mallard::world::World;
+use mallard::world::{World, WorldCommand, WorldResponse};
+use tokio::sync::mpsc;
+// use std::time::Duration;
+// use tokio::time::delay_for;
 
 /// railsim binary for testing instantiation of the various components
 /// To be used as a template for later users.
-fn main() {
-    let temp_world = World;
-    temp_world.temp("blah".to_owned());
+#[tokio::main]
+async fn main() {
+    // Set up the communications with the world object
+    // Channel for sending message from the main thread to the world
+    let (mut main_sender, world_receiver) = mpsc::channel::<WorldCommand>(100);
+
+    // Channel for sending messages from the world to the main thread
+    let (world_sender, mut main_receiver) = mpsc::channel::<WorldResponse>(100);
+
+    // Create our world object with the send and receive channels
+    let temp_world = World {
+        command_ch: world_receiver,
+        response_ch: world_sender,
+    };
+
+    // Start the world
+    let world_thread = tokio::spawn(World::run(temp_world));
+
+    // Wait for the world to be ready
+    println!("Seeing if world is ready");
+    main_sender.send(WorldCommand::AreYouReady).await.unwrap();
+
+    // Check that the world is ready
+    match main_receiver.recv().await {
+        Some(WorldResponse::WorldReady) => println!("World is ready"),
+        Some(_) => {}
+        None => {}
+    }
+
+    // Send the quit command to the world
+    main_sender.send(WorldCommand::Quit).await.unwrap();
+
+    // Loop through until we get a Quit Message.
+    loop {
+        match main_receiver.try_recv() {
+            Ok(WorldResponse::ProcessedQuit) => {
+                println!("Got ProcessedQuit from world, quitting");
+                break;
+            }
+            Ok(WorldResponse::WorldReady) => {
+                println!("World is ready");
+            }
+            Err(_) => {}
+        }
+    }
+
+    world_thread.await.unwrap();
 }
--- src/world/mod.rs
+++ src/world/mod.rs
@@ -1,7 +1,59 @@
-pub struct World;
+/// world module - implements the asynchronous 'real' world of the running trains
+use tokio::sync::mpsc::{Receiver, Sender};
+
+/// World messaging enums
+
+/// Commands to be sent to the world
+#[derive(Debug)]
+pub enum WorldCommand {
+    AreYouReady,
+    Quit,
+}
+
+/// Responses from world commands
+#[derive(Debug)]
+pub enum WorldResponse {
+    WorldReady,
+    ProcessedQuit,
+}
+
+/// World type, handles running the world we run trains in.
+#[derive(Debug)]
+pub struct World {
+    pub command_ch: Receiver<WorldCommand>,
+    pub response_ch: Sender<WorldResponse>,
+}
 
 impl World {
-    pub fn temp(&self, test: String) {
-        println!("{}", test);
+    /// Main Async world function - this runs our train world
+    pub async fn run(mut world: World) {
+        loop {
+            // Get message from the main thread
+            match world.command_ch.try_recv() {
+                // Respond to query asking for our readiness
+                Ok(WorldCommand::AreYouReady) => {
+                    println! {"Sending back world is ready"};
+                    world
+                        .response_ch
+                        .send(WorldResponse::WorldReady)
+                        .await
+                        .unwrap();
+                }
+
+                // Respond to message to shut down world.
+                Ok(WorldCommand::Quit) => {
+                    println!("Got a quit message");
+                    world
+                        .response_ch
+                        .send(WorldResponse::ProcessedQuit)
+                        .await
+                        .unwrap();
+                    break;
+                }
+
+                // Something has gone wrong..
+                Err(_) => {}
+            }
+        }
     }
 }
